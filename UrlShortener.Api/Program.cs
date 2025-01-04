7ddd31bb5ed8c5df8346bd0358aa85e0d6283eec
Richard Lasjunies <rlasjunies@gmail.com>
2025-01-04 10:52:32 +0100

    clean weather forecast root

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -44,29 +44,6 @@ app.MapPost("/api/urls",
         return Results.Created($"/api/urls/{result.Value!.ShortUrl}", result.Value);
     });
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/", () => "Hello, World!");
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-        .ToArray();
-    return forecast;
-});
+app.MapGet("/", () => "UrlShortener Hello, World!");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
